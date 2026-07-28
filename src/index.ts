3005a10a454e4eaed211afcc1cfe441ab3381521
UndefinedRolls <undefinedrolls@gmail.com>
2026-07-28 14:13:08 -0600

    stuff and stuff

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,40 @@ function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
     next();
 }
 
+function handlerValidation(req: Request, res: Response) {
+    const INVALID_WORDS = ["KERFUFFLE", "SHARBERT", "FORNAX"];
+    try
+    {
+        const request = req.body;
+        if (request.body.length > 140)
+        {
+            res.status(400).send(`{"error": "Chirp is too long"}`);
+            return;
+        }
+        const replacement:string = "****";
+        const words:string[] = request.body.split(" ");
+        let clean:string[] = []
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i].toUpperCase();
+
+            if (INVALID_WORDS.includes(word)) {
+                clean.push(replacement);
+            }
+            else{
+                clean.push(words[i]);
+            }
+        }
+
+        res.status(200).send(`{"cleanedBody": "${clean.join(' ')}"}`);
+        return;
+    }
+    catch(error)
+    {
+        res.status(500).send(`${error}: "something went wrong"`);
+        return;
+    }
+}
+
 app.get("/api/healthz", handlerReadiness);
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
@@ -51,6 +85,8 @@ app.get("/admin/metrics", handlerRequests);
 app.use("/admin/metrics", express.static("./admin/metrics"));
 app.post("/admin/reset", handlerReset);
 app.use("/admin/reset", express.static("./api/reset"));
+app.post("/api/validate_chirp", express.json(), handlerValidation);
+app.use("/api/validate_chirp",  express.static("./api/validate_chirp"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 })
